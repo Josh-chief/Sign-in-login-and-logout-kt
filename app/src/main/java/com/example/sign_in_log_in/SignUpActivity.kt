e72166f43498bdeec12dab5d269b8b1862423aa7
josh-chief <yvoshngumi@gmail.com>
2022-11-18 03:13:39 +0300

    start

---
+++ app/src/main/java/com/example/sign_in_log_in/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sign_in_log_in
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
